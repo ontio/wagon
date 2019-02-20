494fde33aa87ff73c8495926a209f4c213e956e6
Zhou Peiwen <zhoupw@gmail.com>
2019-02-20 15:57:46 +0800

    vm: remove float type and related testcase (#6)

--- wasm/operators/const.go
+++ wasm/operators/const.go
@@ -11,6 +11,6 @@ import (
 var (
 	I32Const = newOp(0x41, "i32.const", nil, wasm.ValueTypeI32)
 	I64Const = newOp(0x42, "i64.const", nil, wasm.ValueTypeI64)
-	F32Const = newOp(0x43, "f32.const", nil, wasm.ValueTypeF32)
-	F64Const = newOp(0x44, "f64.const", nil, wasm.ValueTypeF64)
+	//F32Const = newOp(0x43, "f32.const", nil, wasm.ValueTypeF32)
+	//F64Const = newOp(0x44, "f64.const", nil, wasm.ValueTypeF64)
 )
