9eddce5f584c9f890585517008e53458746d0a1b
Zhou Peiwen <zhoupw@gmail.com>
2019-03-06 20:18:57 +0800

    vm:add memory limitation (#7)
    * vm:add memory limitation
    
    * add MemAllocated for process

--- exec/memory.go
+++ exec/memory.go
@@ -6,6 +6,7 @@ package exec
 
 import (
 	"errors"
+	"fmt"
 	"math"
 )
 
@@ -209,6 +210,11 @@ func (vm *VM) growMemory() {
 	_ = vm.fetchInt8() // reserved (https://github.com/WebAssembly/design/blob/27ac254c854994103c24834a994be16f74f54186/BinaryEncoding.md#memory-related-operators-described-here)
 	curLen := len(vm.memory) / wasmPageSize
 	n := vm.popInt32()
+
+	if uint64(len(vm.memory))+uint64(n*wasmPageSize) > vm.MemoryLimitation {
+		panic(fmt.Errorf("growMemory exceed the limitation of %d", vm.MemoryLimitation))
+	}
+
 	vm.memory = append(vm.memory, make([]byte, n*wasmPageSize)...)
 	vm.pushInt32(int32(curLen))
 }
